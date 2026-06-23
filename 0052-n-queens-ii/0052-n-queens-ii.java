e540f3ba4d28d1a62464ca20bcb5c28e0174a1f0
sahajpreet-singhh <sahaj2716@gmail.com>
2026-06-24 01:06:32 +0530

    Time: 8 ms (6.24%), Space: 43.8 MB (5.43%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,75 @@
+class Solution {
+    public int totalNQueens(int n) {
+        char[][] board = new char[n][n];
+        for(int i = 0 ; i< n ; i++){
+            for(int j= 0 ; j < n ; j++){
+                board[i][j] = '.';
+            }
+        }
+        List<List<String>> ans = new ArrayList<>();
+        nqueens(board,0,ans);
+        return ans.size();
+    }
+        private boolean isSafe(char[][] board , int row , int col){
+        int  n = board.length;
+        for(int i = 0 ; i < n ; i++){
+            if(board[row][i] == 'Q') return false;
+        }
+        for(int i = 0 ; i < n ; i++){
+            if(board[i][col] == 'Q') return false;
+        }
+        int i = row;
+        int j = col;
+        while(i < n && j < n){
+            if(board[i][j] == 'Q') return false;
+            i++;
+            j++;
+        }
+        i = row;
+        j = col;
+        while(i >= 0 && j >= 0){
+            if(board[i][j] == 'Q') return false;
+            i--;
+            j--;
+        }
+        i = row;
+        j = col;
+        while(i < n && j >= 0){
+            if(board[i][j] == 'Q') return false;
+            i++;
+            j--;
+        }
+        i = row;
+        j = col;
+        while(i >= 0 && j < n){
+            if(board[i][j] == 'Q') return false;
+            i--;
+            j++;
+        }
+        return true;
+
+    }
+    private void nqueens(char[][] board , int row , List<List<String>> ans){
+        int n = board.length;
+        if(row == n){
+            List<String> l = new ArrayList<>();
+            for(int i = 0 ; i < n ; i++){
+                String str = "";
+                for(int j = 0 ; j < n ; j++){
+                    str += board[i][j];
+                }
+                l.add(str);
+            }
+            ans.add(l);   
+            return;
+        }
+        for(int j = 0 ; j < n ; j++){
+            if(isSafe(board ,row ,j)){
+                board[row][j] = 'Q';
+                nqueens(board,row+1,ans);
+                board[row][j] = '.';
+            }
+        }
+
+    }
+}
