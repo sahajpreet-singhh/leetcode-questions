bb9c63f68f5a6d72331e7fcff4800a4c8d4d9659
sahajpreet-singhh <sahaj2716@gmail.com>
2026-08-05 00:56:11 +0530

    Time: 8 ms (24.61%), Space: 47.3 MB (34.01%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean canFinish(int n, int[][] pre) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0 ; i < n ; i++)adj.add(new ArrayList<>());
+        int[] indegree = new int[n];
+        for(int i = 0 ; i < pre.length ; i++){
+            int a = pre[i][0] , b = pre[i][1];
+            adj.get(b).add(a);
+            indegree[a]++;
+        }
+        //kahn's algo 
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> arr = new ArrayList<>();
+        for(int i = 0 ; i < indegree.length ; i++){
+            if(indegree[i] == 0){
+                q.add(i);
+            }
+        }
+        while(q.size() != 0){
+            int front = q.remove();
+            arr.add(front);
+            for(int ele : adj.get(front)){
+                indegree[ele]--;
+                if(indegree[ele] == 0){
+                    q.add(ele);
+                }
+            }
+        }
+        if(arr.size() == n)return true;
+        else return false;
+
+    }
+}
