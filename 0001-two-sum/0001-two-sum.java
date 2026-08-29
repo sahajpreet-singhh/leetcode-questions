7af0f17d74f49c954f82fc6cc29fd67647fb0577
sahajpreet-singhh <sahaj2716@gmail.com>
2026-08-29 22:59:27 +0530

    Time: 2 ms (99.34%), Space: 47.2 MB (40.97%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -5,7 +5,6 @@ class Solution {
        for(int i= 0 ; i < nums.length ; i++){
         int complement = target - nums[i];
         if(mp.containsKey(complement)){
-            
             arr[0] = mp.get(complement);
             arr[1] = i;
             return arr;
